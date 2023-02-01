e1d423245b0cddcf526433821afeaa3690b4b86a
leshless <Leshuk2104@gmail.com>
2023-02-01 21:29:21 +0300

    12121 21 й

--- Test/js/ui.js
+++ Test/js/ui.js
@@ -1,10 +1,20 @@
-let closed = false
-function changeSidebar(){
-    console.log("OK")
-    if (closed){
+let closedleft = false
+let closedright = false
+function resizeSidebarLeft(){
+    if (closedleft){
         document.getElementById("left-sidebar").style.width = "13%";
     }else{
         document.getElementById("left-sidebar").style.width = "0px";
     }
-    closed = !closed
+    closedleft = !closedleft;
+}
+function resizeSidebarRight(){
+    if (closedright){
+        document.getElementById("right-sidebar").style.width = "13%";
+        document.getElementById("right-sidebar-button").style.right = "13%";
+    }else{
+        document.getElementById("right-sidebar").style.width = "0px";
+        document.getElementById("right-sidebar-button").style.right = "0";
+    }
+    closedright = !closedright;
 }
